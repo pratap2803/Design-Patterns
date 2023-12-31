7f291fcb1d96932bd0b8a7b8352293a6383221d8
pratap2803 <saurabhsinghchauhan1507@gmail.com>
2023-12-31 18:47:46 +0530

    intial commit

---
+++ src/UseCasesOfLLD/DesignSplitwise/Main.java
@@ -0,0 +1,10 @@
+package UseCasesOfLLD.DesignSplitwise;
+
+public class Main {
+    public static void main(String args[]){
+
+        Splitwise splitwise = new Splitwise();
+        splitwise.demo();
+    }
+
+}
